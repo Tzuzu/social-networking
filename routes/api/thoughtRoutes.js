d5b9700ed98c11509c0ada3ba5dd50672fceacaa
Tzuzu <natoiodice@gmail.com>
2023-08-24 00:45:24 -0700

    Fixed routes files and added comments

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -8,17 +8,31 @@ const {
     deleteThought,
     createReaction,
     deleteReaction
-} = require('../../controllers/thoughtController');
+} = require('../../controllers/thoughtController.js');
+
+// /thoughts/
+
+// Displays all thougths if you use GET, creates a new thought if you use POST
 
 router.route('/').get(getThoughts).post(createThought);
 
+// Displays a single thought using GET
+
 router.route('/:thoughtId').get(getSingleThought);
+
+// Updates a single thought using PUT
 
 router.route('/:thoughtId').put(updateThought);
 
+// Deletes a single thought using DELETE
+
 router.route('/:thoughtId').delete(deleteThought);
 
+// Adds a reaction to an existing thought using POST
+
 router.route('/:thoughtId/reactions/:reactionId').post(createReaction);
+
+// Deletes a reaction to an existing thought using DELETE
 
 router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -8,17 +8,31 @@ const {
     deleteUser,
     addFriend,
     deleteFriend
-} = require('../../controllers/userController');
+} = require('../../controllers/userController.js');
+
+// /users/
+
+// Displays all users if you use GET, creates a new user if you use POST
 
 router.route('/').get(getUsers).post(createUser);
 
+// Displays a single user using GET
+
 router.route('/:userId').get(getSingleUser);
+
+// Updates a single user using PUT
 
 router.route('/:userId').put(updateUser);
 
+// Deletes a single user using DELETE
+
 router.route('/:userId').delete(deleteUser);
 
+// Adds a new friend to a user using POST
+
 router.route('/:userId/friends/:friendId').post(addFriend);
+
+// Deletes a friend from a user using DELETE
 
 router.route('/:userId/friends/:friendId').delete(deleteFriend);
 
